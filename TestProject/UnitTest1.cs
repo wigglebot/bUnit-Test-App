abb90ce89595ba21da2640bc68fc857a0bb49679
Josh <38717981+wigglebot@users.noreply.github.com>
2024-02-05 23:51:39 +0100

    Added ASP.NET Core Blazor app & Test project

---
+++ TestProject/UnitTest1.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace Bunit.Docs.Samples;
+
+public class HelloWorldExplicitContext
+{
+  [Test]
+  public void HelloWorldComponentRendersCorrectly()
+  {
+    // Arrange
+    using var ctx = new Bunit.TestContext();
+
+    // Act
+    var cut = ctx.RenderComponent<HelloWorld>();
+
+    // Assert
+    cut.MarkupMatches("<h1>Hello world from Blazor</h1>");
+  }
+}
